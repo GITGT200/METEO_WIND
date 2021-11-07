54e0ba5f04c7334c34d5e2fb959ec50c45d14fff
TibTour <91606647+TibTour@users.noreply.github.com>
2021-11-07 08:36:58 +0100

    Add files via upload

--- Meteo_wind_streamlit.py
+++ Meteo_wind_streamlit.py
@@ -4,8 +4,15 @@ Created on Sat Nov  6 14:11:25 2021
 
 @author: thiba
 """
-import streamlit as st
+import netCDF4 as nc
+import pandas as pd
+import xarray as xr
+import numpy as np
+import csv
 
+
+import streamlit as st
+import netCDF4
 import pandas as pd
 import matplotlib.pyplot as plt
 import folium
@@ -26,7 +33,117 @@ dif_parti=["Carte des vents","Batimétrie marine", \
            "Faune protégée",'Flore protégée']
 Partie=st.sidebar.radio(' ',options=dif_parti)
 
-df=pd.read_csv('1978_2021.csv')
+from_year = st.sidebar.number_input("A partir de l'année", value=2020)
+to_year = st.sidebar.number_input("Jusqu'à de l'année", value=2021)
+north = st.sidebar.number_input('Latitude Nord', value=44.0)
+south = st.sidebar.number_input('Latitude Sud', value=42.0)
+east = st.sidebar.number_input('Longitude Est', value=8.0)
+west = st.sidebar.number_input('Longitude Ouest', value=6.0)
+
+
+#Parameter:
+product='reanalysis-era5-single-levels-monthly-means'
+parameters=['100m_u_component_of_wind',
+            '100m_v_component_of_wind',
+            ]
+
+from_month=1
+to_month=12
+
+if __name__ == '__main__':
+
+    import cdsapi
+    import xarray as xr
+    c = cdsapi.Client()
+
+    years=[]
+    for year in range(from_year, to_year+1):
+        years.append(str(year))
+    months=[]
+    for month in range(from_month, to_month+1):
+        if month <= 9:
+            months.append('0'+str(month))
+        else:
+            months.append(str(month))
+
+    c.retrieve(product,
+        {
+            'format': 'netcdf',
+            'product_type': 'monthly_averaged_reanalysis',
+            'variable': parameters,
+            'year': years,
+            'month': months,
+            'time': '00:00',
+            'area': [
+                north, west, south, east,
+            ],
+        },
+        str(from_year)+'_'+str(to_year)+'.nc')
+
+
+#-------Extration des données et stockage que format csv-----------#
+# Transformation des données en dataframe mais cette solution est mis de coté pour le moment 
+#ds0 = xr.open_dataset(fn)
+#df = ds0.to_dataframe()
+# stockage que format csv
+def ncdfToCsv(ncdffile, outputfilename):
+  """
+  :param ncdffile: path to data in ncdf format. The file is from cds data producer
+  :type ncdffile: string
+  """
+  ds = nc.Dataset(ncdffile)
+
+  lats = ds.variables['latitude'][:]  
+  lons = ds.variables['longitude'][:]
+  time = ds.variables['time'][:]
+  v_wind = ds.variables['v100'][:]
+  u_wind = ds.variables['u100'][:] 
+
+  np_lats = np.ones((lats.shape[0], lons.shape[0]))
+  np_lons = np.ones((lats.shape[0], lons.shape[0]))
+
+  i=0
+  for (lat)  in (lats):
+      #print(lat)
+      np_lats[i, :] = lat;
+      #print(np_lats)
+      i+= 1
+  i=0
+  for (lon)  in (lons):
+      #print(lon)
+      np_lons[:, i] = lon
+      #print(np_lons)
+      i+= 1
+
+  with open(outputfilename, 'w', encoding='UTF8') as f:
+      writer = csv.writer(f)
+      header = ['time', 'lon', 'lat', 'v', 'u']
+      # write the header
+      writer.writerow(header)
+      #file file with data
+      num_mois=0
+      for mois in time:
+          V = v_wind[num_mois][0]
+          U = u_wind[num_mois][0]
+          for num_lat in range(np_lats.shape[0]):
+              for num_lon in range(np_lons.shape[1]):
+                  lon = np_lons[num_lat, num_lon]
+                  lat = np_lats[num_lat, num_lon]
+                  v = V[num_lat, num_lon]
+                  u = U[num_lat, num_lon]
+                  if (u!="--" or v!="--"):
+                      #print(u, v)
+                      # write the data
+                      data = [mois, lon, lat, v, u]
+                      writer.writerow(data)
+    
+          num_mois +=1;
+
+
+ncdfToCsv(str(from_year)+'_'+str(to_year)+'.nc', str(from_year)+'_'+str(to_year)+'.csv')
+df=pd.read_csv(str(from_year)+'_'+str(to_year)+'.csv')
+
+#df=pd.read_csv('data_tibaut.csv')
 df = df[df["u"] != "--"] #on élimine les '--' contenus sur certaines lignes
 df["u"] = df["u"].astype(float)
 df["v"] = df["v"].astype(float)
@@ -37,13 +154,13 @@ if Partie==dif_parti[0]:
     st.title("Carte des vents")
     st.info("Dans cette section, nous voyons la carte des vents.")
     
-    latitude_sup = st.number_input('Latitude supérieure', value=44.0)
-    latitude_inf = st.number_input('Latitude inférieure', value=42.0)
-    longitude_sup = st.number_input('Longitude supérieure', value=8.0)
-    longitude_inf = st.number_input('Longitude inférieure', value=6.0)
+    # latitude_sup = st.number_input('Latitude supérieure', value=44.0)
+    # latitude_inf = st.number_input('Latitude inférieure', value=42.0)
+    # longitude_sup = st.number_input('Longitude supérieure', value=8.0)
+    # longitude_inf = st.number_input('Longitude inférieure', value=6.0)
     
-    df = df[(df["lat"] <= latitude_sup) & (df["lat"] >= latitude_inf) &\
-        (df["lon"] <= longitude_sup) & (df["lon"] >= longitude_inf)]
+    df = df[(df["lat"] <= north) & (df["lat"] >= south) &\
+        (df["lon"] <= east) & (df["lon"] >= west)]
 
     x = df["lon"]
     y = df["lat"]
